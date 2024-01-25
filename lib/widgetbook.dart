cbbce41419a51c8ef4238c5677f46394312f55a8
Jiyun Yang <jiyuunyang@gmail.com>
2024-01-25 16:48:39 +0900

    add: add greenContainerUseCase

--- lib/widgetbook.dart
+++ lib/widgetbook.dart
@@ -1,18 +1,27 @@
+// widgetbook.dart
+
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+// Import the generated directories variable
+import 'widgetbook.directories.g.dart';
 
 void main() {
-  runApp(const HotReload());
+  runApp(const WidgetbookApp());
 }
 
-class HotReload extends StatelessWidget {
-  const HotReload({super.key});
+// The @App annotation generates a file containing
+// a single variable called directories.
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Widgetbook.material(
-      addons: [],
-      directories: [],
+      // Use the generated directories variable
+      directories: directories,
     );
   }
 }

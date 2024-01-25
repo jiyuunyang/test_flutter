d22baf2d96009f6432f851f87a730459198615b1
Jiyun Yang <jiyuunyang@gmail.com>
2024-01-25 16:30:27 +0900

    add: add widgetbook in test project

---
+++ lib/widgetbook.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+void main() {
+  runApp(const HotReload());
+}
+
+class HotReload extends StatelessWidget {
+  const HotReload({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      addons: [],
+      directories: [],
+    );
+  }
+}
